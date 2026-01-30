db3e67d1c3d85afec438fd9c60c42d0160ba21ff
Aurora-Fazliu <auroraafazliu@gmail.com>
2026-01-30 14:46:18 +0100

    Update Property.php

--- classes/Property.php
+++ classes/Property.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../config/Database.php';
+require_once __DIR__ . '/../config/Database.php'; // shkon një nivel lart dhe pastaj në config
 
 class Property {
     private $conn;
@@ -35,10 +35,3 @@ class Property {
     }
 }
 ?>
-
-
-
-
-
-
-
