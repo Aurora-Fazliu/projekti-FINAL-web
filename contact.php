c306e3742af51c6b6787c2cef9fbc149d8fd8b41
Aurora-Fazliu <auroraafazliu@gmail.com>
2026-01-30 14:40:19 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,5 @@
 <?php
-require_once '../classes/Contact.php';
-$msg = "";
+require_once 'classes/Contact.php';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $c = new Contact();
@@ -17,8 +16,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Kontakt – Real Estate Insight</title>
-<link rel="stylesheet" href="assets/style.css">
-<script src="assets/validate.js"></script>
+<link rel="stylesheet" href="style.css">
+<script src="public/assets/validate.js" defer></script>
 </head>
 <body>
 <header>
